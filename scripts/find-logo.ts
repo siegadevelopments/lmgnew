d2a916d7995e51cc45f7741d6e2cb88295874459
Ernest Joseph Siega <siegaej@gmail.com>
2026-04-23 15:15:14 +0800

    Cleanup: Remove unnecessary migration scripts, lockfiles, and configuration

--- scripts/find-logo.ts
+++
@@ -1,9 +0,0 @@
-const https = require('https');
-https.get('https://lifestylemedicinegateway.com', (res: any) => {
-  let data = '';
-  res.on('data', (chunk: any) => data += chunk);
-  res.on('end', () => {
-    const match = data.match(/<img[^>]+class="custom-logo"[^>]*src="([^"]+)"/);
-    if (match) console.log(match[1]);
-  });
-});
