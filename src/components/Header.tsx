226ab1180caa48ff82ef05ff2348a93e2ba5ae7a
Ernest Joseph Siega <siegadevelopments@gmail.com>
2026-05-09 17:43:19 +0800

    fix: add missing Image import in Header to resolve prerender error

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter, usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { useState, useRef, useEffect } from "react";
