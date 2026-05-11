7c23e80e880ebda9ebda29edc8b3ec554c3d7dc9
Ernest Joseph Siega <siegadevelopments@gmail.com>
2026-05-11 15:51:20 +0800

    fix: allow image optimization for Wix domains

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'www.lifestylemedicinegateway.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'static.wixstatic.com',
+      },
     ],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
