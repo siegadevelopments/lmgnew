65a8ab863afdf9d3a8976f3e4e83a930ded16e56
Ernest Joseph Siega <siegadevelopments@gmail.com>
2026-05-25 22:51:20 +0800

    fix: allow cloudflare r2 domains under next.js remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -28,6 +28,10 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: '**.r2.dev',
+      },
+      {
+        protocol: 'https',
         hostname: '**.lifestylemedicinegateway.com',
       },
       {
